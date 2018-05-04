c395908fa270f7c49f1b00773d5b6c3270a6956b
bianyuanyuan <2714540821@qq.com>
2018-05-04 23:34:43 +0800

    modify fragmentmakedate

--- app/src/main/java/com/myapp/Fragment/MakeDataActiviry.java
+++ app/src/main/java/com/myapp/Fragment/MakeDataActiviry.java
@@ -110,8 +110,6 @@ public class MakeDataActiviry extends BaseActivity {
                     startActivity(i);
 
                 //autoStartActivity(CoachListActivity.class);
-
-
             }
         });
 
